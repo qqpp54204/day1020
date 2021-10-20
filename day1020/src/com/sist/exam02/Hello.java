a679dbbbfd05ac72ecff001b18592396c4e63164
qqpp54204 <liver_pool9@naver.com>
2021-10-20 14:41:30 +0900

    하윙

--- day1020/src/com/sist/exam02/Hello.java
+++ day1020/src/com/sist/exam02/Hello.java
@@ -1,10 +1,18 @@
 package com.sist.exam02;
 
+import java.util.Scanner;
+
 public class Hello {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("hello");
+		Scanner sc = new Scanner(System.in);
+		String string;
+		System.out.println("문자를 입력하세요.");
+		string = sc.next();
+		string.replace("안녕", "hello");
+		System.out.println(string);
+		
 	}
 
 }
